f6212deafd4cf35863dff6bc076ba395bca2ccb3
Jandson Costa <jandsoncosta55@gmail.com>
2022-11-24 18:59:33 -0300

    save express

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 
 
 const express = require('express');
+
  
-const app = express()
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/home', (request, response, next) => {
+    console.log(request);
+    response.send('<h1>Welcome Ironhacker. :)</h1>');
+  });
+
+
+  app.listen(3000, () => console.log('My first app listening on port 3000! '));
